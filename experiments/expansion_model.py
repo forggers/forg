1bb7518e5b2d86f090d24b085df943e4fb23ac4f
Alex Chen <axchen7@outlook.com>
2024-12-10 01:43:11 +0000

    Add expansion_model experiment

---
+++ experiments/expansion_model.py
@@ -0,0 +1,12 @@
+from forg.train import train
+
+for expansion_model_name in ["google/gemma-2-2b", "meta-llama/Meta-Llama-3-8B"]:
+    for run in range(3):  # run multiple times
+        train(
+            "../data/repos/react",
+            run_label=f"expansion_model-{expansion_model_name}-run{run}",
+            samples=3000,
+            epochs=100_000,
+            expansion_model_name=expansion_model_name,
+            expansion_batch_size=64,
+        )
