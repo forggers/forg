2decd217b1a1dd43ba9f27e7ff20eca49bbc2a5d
Alex Chen <axchen7@outlook.com>
2024-12-13 17:04:13 -0500

    Only take regular files

--- forg/utils.py
+++ forg/utils.py
@@ -37,7 +37,8 @@ def load_files(repo_dir: str) -> list[RawFile]:
     for dir, _, files in os.walk(repo_dir):
         for file in files:
             path = os.path.join(dir, file)
-            raw_files.append(RawFile(path=path, repo_dir=repo_dir))
+            if os.path.isfile(path):  # Only take regular files
+                raw_files.append(RawFile(path=path, repo_dir=repo_dir))
 
     return raw_files
 
